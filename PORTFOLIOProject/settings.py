2370fdd7220f814559b764922715471cc9b4d5dc
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-04-24 12:59:19 +0100

    third commit

--- PORTFOLIOProject/settings.py
+++ PORTFOLIOProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gg30)^7#8tu#5%(&vop7&ap&j(k#gp4em#aa32f(y1o%16549a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['binportfoliomark2.onrender.com', 'localhost']
 
 
 # Application definition
