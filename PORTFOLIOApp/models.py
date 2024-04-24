e419b74d211c1f3981d820fc1e8ca1db2e798bd1
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-04-24 12:49:51 +0100

    first commit

---
+++ PORTFOLIOApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class ContactMe(models.Model):
+    name = models.CharField(max_length=225, blank=True)
+    email = models.EmailField(unique=True, max_length=225, blank=True)
+    subject = models.CharField(max_length=225, blank=True)
+    message = models.TextField(max_length=225, blank=True)
+    sent_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email, self.subject
